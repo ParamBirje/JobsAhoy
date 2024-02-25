5e0b004d869d17b0a7b950c8752b89ab6474f867
Param Birje <parambirje@gmail.com>
2024-02-25 18:01:43 +0530

    Restructured local components misplaced in /src/components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Footer from "@/components/Footer";
-import CallToAction from "../components/LandingPage/CallToAction";
-import Hero from "../components/LandingPage/Hero";
-import Services from "../components/LandingPage/Services";
-import Testimonials from "../components/LandingPage/Testimonials";
+import CallToAction from "./_components/CallToAction";
+import Hero from "./_components/Hero";
+import Services from "./_components/Services";
+import Testimonials from "./_components/Testimonials";
 
 export default function Home() {
   return (
