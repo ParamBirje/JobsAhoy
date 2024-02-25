da5fb6acfb477b233aa5336c3aea0bf59dd0bd97
Param Birje <parambirje@gmail.com>
2024-02-25 18:55:10 +0530

    Added a basic element structure to HowItWorks on landing page

--- src/app/_components/HowItWorks.tsx
+++ src/app/_components/HowItWorks.tsx
@@ -1,3 +1,26 @@
 export default function HowItWorks() {
-  return <div>HowItWorks</div>;
+  return (
+    <section id="hero" className="max-w-7xl mx-auto">
+      <div className="flex flex-col gap-20">
+        <h3 className="text-center text-5xl font-bold">How It All Works</h3>
+
+        <div className="flex flex-col gap-10">
+          <div className="flex items-center justify-center gap-5">
+            <div>Image</div>
+            <div>Step1</div>
+          </div>
+
+          <div className="flex items-center justify-center gap-5">
+            <div>Image</div>
+            <div>Step2</div>
+          </div>
+
+          <div className="flex items-center justify-center gap-5">
+            <div>Image</div>
+            <div>Step3</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
