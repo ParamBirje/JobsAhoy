cb0cf72bc58bdfb4df8f696dd3439cefc17ea87d
Param Birje <parambirje@gmail.com>
2024-02-25 18:39:48 +0530

    Implemented the Companies section on Landing Page

--- src/app/_components/CompaniesBar.tsx
+++ src/app/_components/CompaniesBar.tsx
@@ -1,3 +1,29 @@
+import Netflix from "@/lib/images/companyLogos/Netflix.png";
+import Adobe from "@/lib/images/companyLogos/Adobe.png";
+import Google from "@/lib/images/companyLogos/Google.png";
+import Miro from "@/lib/images/companyLogos/Miro.png";
+import Amazon from "@/lib/images/companyLogos/Amazon.png";
+import Image from "next/image";
+
 export default function CompaniesBar() {
-  return <div>CompaniesBar</div>;
+  const images = [Netflix, Adobe, Google, Miro, Amazon];
+  const logoSizeReduction = 40;
+
+  return (
+    <section id="companies" className="bg-primary-light px-5 relative">
+      <div className="flex justify-evenly items-center py-10">
+        {images.map((image, idx) => {
+          return (
+            <Image
+              key={idx}
+              src={image}
+              alt={`${image.src}-logo`}
+              height={image.height - logoSizeReduction}
+              width={image.width - logoSizeReduction}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
