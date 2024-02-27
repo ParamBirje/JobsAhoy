164b122e0be4b049f8e600524ca47496bd615cea
Param Birje <parambirje@gmail.com>
2024-02-27 18:17:51 +0530

    Tweaked email login testing area UI, deleted /user/create-account route

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -12,10 +12,10 @@ export default async function SignIn() {
 
   return (
     <main className="max-w-7xl mx-auto px-5 flex items-center justify-center min-h-[80vh]">
-      <div className="w-[80%] flex flex-col gap-10">
+      <div className="w-[80%]">
         <div className="mx-auto text-center w-3/4 mb-[4rem]">
           <h1 className="text-[38px] font-extrabold mb-1">
-            Login To Your Account
+            Login / Create Account
           </h1>
           <p className="text-sm text-secondary-dark w-[60%] mx-auto tracking-wide">
             Millions of people have already joined and are searching actively
@@ -25,27 +25,19 @@ export default async function SignIn() {
 
         <div className="flex justify-between items-start gap-[5rem] w-[85%] mx-auto">
           <div className="h-full w-full self-center">
-            <SignInForm />
-
             <div className="w-full my-1 h-full">
-              <Link
-                href="/user/create-account"
-                className="duration-100 flex items-center justify-center gap-2 hover:bg-secondary text-seconday border border-secondary hover:text-primary font-medium tracking-wide py-3 px-4 rounded"
-                type="button"
-              >
-                <p>Quickly Create An Account</p>
-              </Link>
+              <div className="duration-100 flex items-center justify-center gap-2 text-seconday border border-green-400 text-green-400 font-medium tracking-wide py-3 px-4 rounded">
+                <p>Testing Area</p>
+              </div>
             </div>
+
+            <SignInForm />
           </div>
 
           <p className="self-center">/</p>
 
           <SocialLoginButtons />
         </div>
-
-        <button className="text-sm text-secondary-dark underline">
-          Forgot Password?
-        </button>
       </div>
     </main>
   );
