c4b61f2e53efcecde4ba673e2543cc3c8f5f72a0
Param Birje <parambirje@gmail.com>
2024-03-03 18:30:46 +0530

    Updated position of Loading to be in center of screen

--- src/app/jobs/loading.tsx
+++ src/app/jobs/loading.tsx
@@ -2,9 +2,5 @@ import Loading from "@/components/Loading";
 import React from "react";
 
 export default function LocalLoading() {
-  return (
-    <div className="text-center">
-      <Loading />
-    </div>
-  );
+  return <Loading />;
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <p>Loading</p>
+    </div>
+  );
 }
